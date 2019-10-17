8410b2db053fe21d20415ae9e653fe7c4e832806
Diana Mryhlotska <diana.mryhlotska.kn.2016@lpnu.ua>
2019-10-17 16:45:27 +0300

    exercise 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Exercise 1 ------------------------------------");
         MyHashTable hashTable = new MyHashTable(0.5);
         hashTable.insert(1);
         hashTable.insert(3);
@@ -11,6 +12,25 @@ public class Main {
         hashTable.insert(12);
         hashTable.insert(13);
         hashTable.delete(12);
+        System.out.println("End of Exercise 1 ------------------------------------");
+
+        System.out.println("Exercise 2 ------------------------------------");
+
+        System.out.println("End of Exercise 2 ------------------------------------");
+
+        System.out.println("Exercise 3 ------------------------------------");
+
+        MyMeasure measure = new MyMeasure();
+        int array1[] = { 3, 5, 2, 5, 2 };
+        int array2[] = { 2, 3, 5, 5, 2 };
+
+        if (measure.isSameCollection(array1, array2))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+
+
+        System.out.println("End of Exercise 3 ------------------------------------");
 
     }
 }
