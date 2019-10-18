21b89ab2b1da2855f5ffd8588efb78e50c167a3c
Diana Mryhlotska <diana.mryhlotska.kn.2016@lpnu.ua>
2019-10-18 15:58:48 +0300

    update

--- src/MyMeasure.java
+++ src/MyMeasure.java
@@ -1,30 +1,64 @@
-import java.util.Arrays;
 
 public class MyMeasure implements A2Measure {
 
 
-    @Override
-    public boolean isSameCollection(int[] array1, int[] array2){
-        int n = array1.length;
-        int m = array2.length;
+    public static void selectionSort(int[] array){
 
-        if (n != m)
-            return false;
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int min_i = i;
 
-        Arrays.sort(array1);
-        Arrays.sort(array2);
-
-        for (int i = 0; i < n; i++)
-            if (array1[i] != array2[i])
-                return false;
-
-        // If all elements were same.
-        return true;
+            for (int j = i+1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    min_i = j;
+                }
+            }
+            if (i != min_i) {
+                int tmp = array[i];
+                array[i] = array[min_i];
+                array[min_i] = tmp;
+            }
+        }
     }
 
     @Override
-    public int minDifferences(int[] array1, int[] array2){
+    public boolean isSameCollection(int[] array1, int[] array2){
+        int N = array1.length;
+        int M = array2.length;
 
+        if (N != M)
+            return false;
+
+        selectionSort(array1);
+        selectionSort(array2);
+
+        for (int i = 0; i < N; i++)
+            if (array1[i] != array2[i])
+                return false;
+
+        return true;
+    }
+
+
+
+        @Override
+    public int minDifferences(int[] array1, int[] array2){
+        int N = array1.length;
+        int M = array2.length;
+        int arrayStore [];
+
+        if (N == M)
+        {
+            selectionSort(array1);
+            selectionSort(array2);
+            for (int i = 0; i < N; i ++){
+            }
+        }
+        else{
+            System.out.println("The arrays are not the same size!");
+        }
+        return 0;
     }
 
    // public int[] getPercentileRange(int[] arr, int lower, int upper) {}
